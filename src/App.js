388f5fe58762947da7b7f9fd487167f0a1176d76
JEET AHIRWAR <jeetahirwar664@gmail.com>
2024-04-22 10:28:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+// App.js
+import React, { useState, useEffect } from 'react';
+import CryptoTable from './CryptoTable';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [cryptos, setCryptos] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false');
+        const data = await response.json();
+        setCryptos(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>CryptoApp</h1>
+      <CryptoTable cryptos={cryptos} />
     </div>
   );
-}
+};
 
 export default App;
